10b06b9c0a55f9ef1dbc05ac94c3d9e99c9c56ed
luciano <luciano.cardoso@ufabc.edu.br>
2021-08-10 17:47:05 -0300

    v.1.4

--- src/main/java/br/poo/cursed/npc/Heroes.java
+++ src/main/java/br/poo/cursed/npc/Heroes.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.poo.cursed.npc;
 
 import br.poo.cursed.mob.Inimigo;
 
-/**
- *
- * @author lpatinocardoso
- */
 public abstract class Heroes {
     protected double nivel;
     protected String nome;    
